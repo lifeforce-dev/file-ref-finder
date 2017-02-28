5565ad9b7b68be18fa89085b4c74ef8ad0e7f06a
jsandersr <joshua_sandersr@yahoo.com>
2017-02-27 23:56:42 -0800

    Search and ref extensions are now configurable
    - Optionally supply a list of extensions of references
      to search for.
    - Optionally supply a list of directory extensions to
      search through.
    - Root search_dir is now required.
    - A bit of clean up happened

--- find_file_references.py
+++ find_file_references.py
@@ -4,12 +4,17 @@ import re
 import argparse
 import mmap
 
+#/
+# Utility functions
+#/
+
+# Clears stale logs.
 def clear_logs():
 	if os.path.exists(no_reference_log_name):
 		open(no_reference_log_name, 'w').close()
 
-def file_not_empty(file):
-	return os.stat(file).st_size > 0
+def is_file_empty(file):
+	return os.stat(file).st_size <= 0
 
 # Returns if this file is one that we're allowed to look for.
 def file_is_searchable(file):
@@ -25,11 +30,16 @@ def log_no_reference(reference):
 
 	file.write("No references for " + reference + "\n")
 
+#/
+# Work functions
+#/
+
 def build_ref_list(ref_list):
-	# Gather all of the .png filenames and append to our reference list.
+	# Gather all of the files that meet our reference criteria and append to our ref list.
 	for root, dirs, files in os.walk(args.ref_dir):
 		for file in files:
-			if ".png" in file:
+			ext = os.path.splitext(file)[1]
+			if ext in args.ref_extensions:
 				ref_list[file] = False
 
 # Return whether a given regex matches any line in the specified file.
@@ -50,7 +60,7 @@ def mark_references_in_file(root, file):
 		regex_pattern = r'(.*%s.*)' % ref_base_name
 
 		if find_ref_in_file(root, file, regex_pattern):
-			print "Found reference to: " + ref_base_name
+			print "Found reference to: " + ref + " in " + file
 			ref_list[ref] = True
 
 # For every file in the search directory, mark the existence of all refs in the ref_list
@@ -58,28 +68,65 @@ def mark_references():
 	# Determine which files contain at least one reference in any file from the root directory.
 	for root, dirs, files in os.walk(args.search_dir):
 		for file in files:
-			if file_is_searchable(file) and file_not_empty(os.path.join(root, file)):
+			if file_is_searchable(file) and not is_file_empty(os.path.join(root, file)):
 				print "Searching for references in " + file + "..."
 				mark_references_in_file(root, file)
 
+# Look through all of the data
+def dump_results():
+	print "Logging files that had no reference..."
+	for ref, found in ref_list.iteritems():
+		if not found:
+			log_no_reference(ref);
+
 if __name__ == "__main__":
+
 	# Command line args
-	parser = argparse.ArgumentParser(description= 'Find file references in directory')
-	parser.add_argument('-s', '--search_dir', help= 'Root directory of files potentially containing references.')
-	parser.add_argument('-r', '--ref_dir', default= os.getcwd(), help= "Directory containing files we're looking for references of.")
+	parser = argparse.ArgumentParser(description= 'Find file references in directory. '
+		'Default functionality is to find all images referenced in the text of the provided directory tree')
+	required  = parser.add_argument_group('required arguments')
+
+	# search_dir: This is required. We need to know which root directory to search in.
+	required.add_argument('-s', '--search_dir',
+		help= 'Root directory of files potentially containing references.', required = True)
+
+	# Everything below here is optional.
+	# ref_dir. We will assume the files we want references to are in this directory.
+	parser.add_argument('-r', '--ref_dir', default= os.getcwd(),
+		help= "Directory containing files we're looking for references of.")
+
+	# search_extensions: We provide some sort of sane default.
+	parser.add_argument('-f', '--search_extensions', nargs='*',
+		default= [".ui", ".cpp", ".txt", ".json", ".css", ".h", ".qml"],
+		help= 'Only files in this list of extensions will be searched. ex: .txt .cpp .h')
+
+	# ref_extensions: We provide some sort of sane default.
+	parser.add_argument('-e', '--ref_extensions', nargs='*',
+		default= ['.png', '.jpg', '.mng', '.jpeg', '.gif'],
+		help= 'Find references to all files with an extension in this list. ex: .jpg .png .jpeg')
+
 	args = parser.parse_args()
+
+	print args.search_extensions
+	print args.ref_extensions
 
 	# We're searching for references to any of the files in this list.
 	ref_list = {}
-	search_extensions = [".ui", ".cpp", ".txt", ".json", ".css", ".h", ".qml"]
+	search_extensions = args.search_extensions
+	ref_extensions = args.ref_extensions
 
 	# If we return no references to a file name, we will log it here.
 	no_reference_log_name = os.path.join(os.getcwd(), "no-references.log")
 
+	print "Clearing logs..."
 	clear_logs()
+	
+	print "Building list of files you're searching for references of..."
 	build_ref_list(ref_list)
+
+	print "Working..."
 	mark_references()
-	for ref, found in ref_list.iteritems():
-		print ref + " , " + str(found)
-		if not found:
-			log_no_reference(ref);
+
+	dump_results()
+
+	print "done."
