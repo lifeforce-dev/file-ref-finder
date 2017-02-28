66edc2e7d5d601cf8ff8d2a9aeb128020b4c0d13
jsandersr <joshua_sandersr@yahoo.com>
2017-02-27 20:09:00 -0800

    Rewrite algorithm to be more efficient
    - Instead of checking every reference against every file,
      I now check every file against remaining references.
    - Reference existence is now cached. So if a reference is found,
      we no longer have to look for that one in later files
    - ref_list is now a dictionary
    - Algorithm is now:
      - For every eligible file in search_dir
        - For every unfound file in the ref_list
          - If a remaining reference exist in the file text
             - Mark as found
      - Iterate through ref_list and list ones not found.

--- find_file_references.py
+++ find_file_references.py
@@ -8,29 +8,6 @@ def clear_logs():
 	if os.path.exists(no_reference_log_name):
 		open(no_reference_log_name, 'w').close()
 
-def log_no_reference(reference):
-	# Append to end of file if it exists, otherwise create it.
-	if os.path.exists(no_reference_log_name):
-		fh = open(no_reference_log_name, "a")
-	else:
-		fh = open(no_reference_log_name, "w")
-
-	fh.write("No references for " + reference + ".png\n")
-
-def build_ref_list(ref_list):
-	# Gather all of the .png filenames and append to our reference list.
-	for root, dirs, files in os.walk(args.ref_dir):
-		for file in files:
-			if ".png" in file:
-				ref_list.append(file)
-
-def find_ref_in_file(root, file, pattern):
-	with open(os.path.join(root, file), 'r+') as file_in:
-		data = mmap.mmap(file_in.fileno(), 0)
-		mo = re.search(pattern, data)
-		if mo:
-			return True
-
 def file_not_empty(file):
 	return os.stat(file).st_size > 0
 
@@ -39,26 +16,51 @@ def file_is_searchable(file):
 	extension = os.path.splitext(file)[1]
 	return  extension in search_extensions
 
-def find_reference(reference):
-	found = False
-	# Starting at the root, find all lines in every text file containing a reference
+def log_no_reference(reference):
+	# Append to end of file if it exists, otherwise create it.
+	if os.path.exists(no_reference_log_name):
+		file = open(no_reference_log_name, "a")
+	else:
+		file = open(no_reference_log_name, "w")
+
+	file.write("No references for " + reference + "\n")
+
+def build_ref_list(ref_list):
+	# Gather all of the .png filenames and append to our reference list.
+	for root, dirs, files in os.walk(args.ref_dir):
+		for file in files:
+			if ".png" in file:
+				ref_list[file] = False
+
+# Return whether a given regex matches any line in the specified file.
+def find_ref_in_file(root, file, pattern):
+	with open(os.path.join(root, file), 'r+') as file_in:
+		data = mmap.mmap(file_in.fileno(), 0)
+		mo = re.search(pattern, data)
+		if mo:
+			return True
+
+# For each ref in the ref_list, if a match is found in the current file, mark it.
+def mark_references_in_file(root, file):
+	for ref, found in ref_list.iteritems():
+		# Don't bother searching for a reference we've already found before.
+		if found:
+			continue
+		ref_base_name = os.path.splitext(ref)[0]
+		regex_pattern = r'(.*%s.*)' % ref_base_name
+
+		if find_ref_in_file(root, file, regex_pattern):
+			print "Found reference to: " + ref_base_name
+			ref_list[ref] = True
+
+# For every file in the search directory, mark the existence of all refs in the ref_list
+def mark_references():
+	# Determine which files contain at least one reference in any file from the root directory.
 	for root, dirs, files in os.walk(args.search_dir):
 		for file in files:
 			if file_is_searchable(file) and file_not_empty(os.path.join(root, file)):
-				reference = os.path.splitext(reference)[0]
-				regexPattern = r'(.*%s.*)' % reference
-				found = find_ref_in_file(root, file, regexPattern)
-				if found:
-					print "Found reference in " + os.path.join(root, file)
-					return
-	if not found:
-		print "No reference found. Event logged."
-		log_no_reference(reference)
-
-def find_references():
-	for ref in ref_list:
-		print "Finding references for " + ref
-		find_reference(ref)
+				print "Searching for references in " + file + "..."
+				mark_references_in_file(root, file)
 
 if __name__ == "__main__":
 	# Command line args
@@ -68,7 +70,7 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	# We're searching for references to any of the files in this list.
-	ref_list = []
+	ref_list = {}
 	search_extensions = [".ui", ".cpp", ".txt", ".json", ".css", ".h", ".qml"]
 
 	# If we return no references to a file name, we will log it here.
@@ -76,5 +78,8 @@ if __name__ == "__main__":
 
 	clear_logs()
 	build_ref_list(ref_list)
-	find_references()
-
+	mark_references()
+	for ref, found in ref_list.iteritems():
+		print ref + " , " + str(found)
+		if not found:
+			log_no_reference(ref);
